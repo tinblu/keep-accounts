14cdd3660a63b92704b65976a6ccd212e296b7f5
tinblu <detectivedklh@gmail.com>
2023-03-21 18:44:54 +0800

    自动跳转到子路由

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,16 +1,20 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { Outlet, createBrowserRouter } from 'react-router-dom'
 import { RedirectToWelcome1 } from '../components/RedirectToWelcome1'
 import { MainLayout } from '../layouts/MainLayout'
 import { welcomeRoutes } from './welcomeRoutes'
 
 export const router = createBrowserRouter([
   {
+    path: '/',
+    element: <MainLayout />
+  },
+  {
     path: '/home',
     element: <div>home</div>
   },
   {
     path: '/',
-    element: <MainLayout />,
+    element: <Outlet />,
     errorElement: <RedirectToWelcome1 />,
     children: [
       welcomeRoutes
