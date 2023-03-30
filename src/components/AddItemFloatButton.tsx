2aed092e31751f8b8cfc831dac90d156cdb6f14f
tinblu <detectivedklh@gmail.com>
2023-03-30 22:26:04 +0800

    封装 Icon.tsx 组件

--- src/components/AddItemFloatButton.tsx
+++ src/components/AddItemFloatButton.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Icon } from "./Icon";
 
 export const AddItemFloatButton: React.FC = () => {
   return (
-    <button p-4px w-56px h-56px bg="#accbee" rounded="50%" b-none text-white
-       fixed bottom-16px right-16px>
-      <svg style={{ fill: 'red', width: '1.2em', height: '1.2em' }}>
-        <use xlinkHref='#menu'></use>
+    <button p-4px w-56px h-56px  rounded="50%" b-none text-white 
+      fixed bottom-16px right-16px flex justify-center items-center bg-white>
+      <svg style={{ fill: 'red', width: '11.2em', height: '11.2em', color:'#accbee'}}>
+         <Icon name="add" className="w-48px h-48px" />
       </svg>
     </button>
   )
