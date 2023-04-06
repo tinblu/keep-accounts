caa3f650baec19d48c3497d3c2e409714eee781f
tinblu <detectivedklh@gmail.com>
2023-04-07 00:07:36 +0800

    基本完成表单提交功能

--- mock/test.ts
+++ mock/test.ts
@@ -1,7 +1,9 @@
 import { MockMethod } from 'vite-plugin-mock'
 import { itemsMock } from './items.mock'
 import { meMock } from './me.mock'
+import { sessionMock } from './session.mock'
 export default [
   meMock,
   itemsMock,
+  sessionMock
 ] as MockMethod[]
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -3,11 +3,14 @@ import { Gradient } from "../components/Gradient"
 import { Icon } from "../components/Icon"
 import { Topnav } from "../components/Topnav"
 import { useSignInStore } from "../stores/useSignInStore"
-import { validate } from "../lib/validate"
+import { hasError, validate } from "../lib/validate"
+import { ajax } from "../lib/ajax"
+import { useNavigate } from "react-router-dom"
 
 export const SignInPage: React.FC = () =>{
   const { data, error, setData, setError } = useSignInStore()
-  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+  const nav = useNavigate()
+  const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault()
     const error = validate(data, [
       { key: 'email', type: 'required', message: '请输入邮箱地址' },
@@ -16,6 +19,10 @@ export const SignInPage: React.FC = () =>{
       { key: 'code', type: 'length', min: 6, max: 6, message: '验证码必须是6个字符' },
     ])
     setError(error)
+    if(!hasError(error)){
+      await ajax.post('/api/v1/session', data)
+      nav('/home')
+    }
   }
   return (
     <div>
@@ -27,7 +34,6 @@ export const SignInPage: React.FC = () =>{
         <h1 text-32px text="#7878FF" font-bold>bingo记账</h1>
       </div>
       <form j-form onSubmit={onSubmit}>
-        <div b-1 b-red>{JSON.stringify(data)}</div>
         <div>
           <span j-form-label>邮箱地址 {error.email?.[0] && <span text-red>{error.email[0]}</span>}</span>
           <input j-input-text type="text" placeholder='请输入邮箱，然后点击发送验证码' 
